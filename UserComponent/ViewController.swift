f2c1c1734d3811febeac2a925845af9707658434
Startdevelopment <contact@startdevelopment.fr>
2020-09-01 11:54:27 +0100

    Initial Commit

---
+++ UserComponent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserComponent
+//
+//  Created by Startdevelopment on 01/09/2020.
+//  Copyright © 2020 MohamedNouri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
